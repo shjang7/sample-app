c94eb742b7473f4bd0eb94c7876b47aa147b0f7b
shjang7 <lucy.sh.jang@gmail.com>
2019-07-17 06:50:31 +0900

    Add for asking correct form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
