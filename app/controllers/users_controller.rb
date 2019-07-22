33722a48b9c939e1f09e87ba49102fc3cf49508c
shjang7 <lucy.sh.jang@gmail.com>
2019-07-22 10:00:49 +0900

    Add all user list page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user,  only: [:edit, :update]
+  before_action :logged_in_user,  only: [:index, :edit, :update]
   before_action :correct_user,    only: [:edit, :update]
 
-  def new
-    @user = User.new
+  def index
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
