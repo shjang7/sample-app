ca8394673892bcc0c08a9649f48e5b86e732a98a
shjang7 <lucy.sh.jang@gmail.com>
2019-07-23 17:33:44 +0900

    Edit misspelling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :addresses              => 'smtp.sendgrid.net',
     :port                   => '587',
-    :authentication         => :plain
+    :authentication         => :plain,
     :user_name              => ENV['SENDGRID_USERNAME'],
-    :password               => ENV['SENDGRID_PASSWORD'], 
+    :password               => ENV['SENDGRID_PASSWORD'],
     :domain                 => 'heroku.com',
     :enable_starttls_auto   => true
   }
