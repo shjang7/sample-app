8b244ba40d55b5c096377519569776ec90f75049
shjang7 <lucy.sh.jang@gmail.com>
2019-08-01 16:20:37 +0900

    Update config to upload AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,8 @@ if Rails.env.production?
       # Configuration for Heroku
       provider:                 'AWS',                    # required
       aws_access_key_id:        ENV['S3_ACCESS_KEY'],     # required
-      aws_secret_access_key:    ENV['S3_SECRET_KEY']      # required
+      aws_secret_access_key:    ENV['S3_SECRET_KEY'],     # required
+      region:                   ENV['S3_REGION']          # required
     }
     config.fog_directory    =   ENV['S3_BUCKET']          # required
   end
