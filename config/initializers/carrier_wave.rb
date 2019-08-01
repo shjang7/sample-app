69bf792540749cf15321432e46b2d3d7f93ffa30
shjang7 <lucy.sh.jang@gmail.com>
2019-08-01 13:21:39 +0900

    Add image upload in microposts

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Configuration for Heroku
+      provider:                 'AWS',                    # required
+      aws_access_key_id:        ENV['S3_ACCESS_KEY'],     # required
+      aws_secret_access_key:    ENV['S3_SECRET_KEY']      # required
+    }
+    config.fog_directory    =   ENV['S3_BUCKET']          # required
+  end
+end
