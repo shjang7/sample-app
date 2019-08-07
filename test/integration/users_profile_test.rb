49ac3dbe6da560df8c642a6228a682ed042b8de8
shjang7 <lucy.sh.jang@gmail.com>
2019-08-08 00:32:50 +0900

    Add features followed, following in following-users

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,11 +14,20 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
-    assert_select 'h1>img.gravatar'
+    assert_select '.user_info>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+  end
+
+  test "homepage stats" do
+    log_in_as(@user)
+    get root_path
+    # assert_template "shared/_logged_user"
+    assert_select "div.stats"
+    assert_select "strong#following"
+    assert_select "strong#followers"
   end
 end
