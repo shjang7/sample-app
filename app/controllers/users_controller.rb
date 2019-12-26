894fdfbc8e7357b2c52c5d0b20d6ad6d85250baf
shjang7 <lucy.sh.jang@gmail.com>
2019-12-26 19:08:17 +0900

    Temporiry allow user to login without activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
     # @users = User.paginate(page: params[:page])
     # or, use an explicit 'per page' limit:
     # @users = User.paginate(:page => params[:page], :per_page => 30)
-    @users = User.where(activated: true).paginate(page: params[:page])
+    @users = User.paginate(:page => params[:page])
+    # @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
