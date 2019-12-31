902af28039dff251d505dd555616d0d4e8507dc2
shjang7 <lucy.sh.jang@gmail.com>
2019-12-31 21:22:44 +0900

    Update cloud storage directory

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,10 +6,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   if Rails.env.production?
     storage :fog
   else
@@ -19,7 +15,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/sample_app/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
